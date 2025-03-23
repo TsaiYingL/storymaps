cf47a972b398febce0ccbc0b4156d2d9866bbd6c
TsaiYingL <tsaiying11100815@gmail.com>
2025-03-23 15:37:01 -0400

    trying things out

---
+++ home.js
@@ -0,0 +1,38 @@
+//file for the slideshow on the home page
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("screens");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = x.length; }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-white";
+}
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+document.querySelector(".w3-left").onclick = function () { plusDivs(-1); };
+document.querySelector(".w3-right").onclick = function () { plusDivs(1); };
+
+var dots = document.querySelectorAll(".demo");
+dots.forEach((dot, index) => {
+    dot.onclick = function () { currentDiv(index + 1); };
+});
+
+showDivs(slideIndex);
