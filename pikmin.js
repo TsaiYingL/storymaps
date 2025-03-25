ce33b5a4996d5a5f8be53a60ad3b920e7987d220
TsaiYingL <tsaiying11100815@gmail.com>
2025-03-25 01:37:43 -0400

    zoom into map

--- pikmin.js
+++ pikmin.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     //map on pikmin page
-    var map = L.map('map').setView([40.9153, -73.1226], 13);
+    var map = L.map('map').setView([40.9153, -73.1226], 17);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
